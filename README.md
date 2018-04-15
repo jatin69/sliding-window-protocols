0cafd3c63305799cabc9a60e72e14c169b971246
Jatin <jatinrohilla69@gmail.com>
2018-04-16 00:45:37 +0530

    stop and wait almost done - index.html and app.py both formatted and documented - following prettier in js styling - packet numbers and ack numbers done - async time done as well

--- README.md
+++ README.md
@@ -1,54 +1,73 @@
 # mca204-networks
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
 Standard Networking Protocols using [flask socketIO](https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example)
 
+* refactoring
+
+  * [x] index.html
+    * [x] sorted by events
+    * [x] documented
+    * [x] formatted
+    * [X] prettify from separate pretty.js
+  * [X] app.py
+    * [X] sorted by events
+    * [X] documented
+    * [X] formatted
+  * pretty.js
+    * prettier formatting is an acquired taste 
+
+* colors for ack, message, and crashes
+* put exact packet numbers in all messages
+* try sliding - works well - amazing
+* add acknowledgement crashing
+
+* packet crashed - handled
+* ack crashed - to be handled
 
 ## TODO - Stop and Wait
 
-- need to make Packet # and ACK # separate
-- show message : resending >>>> in sender logs 
-- prettify everything with bootstrap
-- give option between custom message & Packet number on button
-- [X] refactor done
-- [X] medium now part of handshake
-- make packets and acknowledgement
-- automate packets as P1, P2, etc. & ack as A1, A2, etc.
-- Middle layer needs to work out - drop ack & message
-- start a clock each time a packet is sent
-- resend the packet if clock is over and acknowledgement is not received
-- put proper long comments
-- to be told about crashing in the medium logs
+* need to make Packet # and ACK # separate
+* show message : resending >>>> in sender logs
+* prettify everything with bootstrap
+* give option between custom message & Packet number on button
+* [x] refactor done
+* [x] medium now part of handshake
+* make packets and acknowledgement
+* automate packets as P1, P2, etc. & ack as A1, A2, etc.
+* Middle layer needs to work out - drop ack & message
+* start a clock each time a packet is sent
+* resend the packet if clock is over and acknowledgement is not received
+* put proper long comments
+* to be told about crashing in the medium logs
 
 ## Major
 
-- status in table format - scrollable
-- threaded timer
-- packet and Ack numbers
-- a lot of events and a lot of time
+* status in table format - scrollable
+* threaded timer
+* packet and Ack numbers
+* a lot of events and a lot of time
 
 ## Transmission Flow
 
-0. Hi sender & Hi receiver - both connected
-1. Packet forms at senderFrontend
-
-2. SendPacketToMiddleLayerBackend
-3. SendPacketToMiddleLayerFrontend
-
-4. SendPacketToReceiverBackend
-5. sendPacketToReceiverFrontend
-
-6. sendAckToMiddleLayerBackend
-7. sendAckToMiddleLayerFrontend
-
-8. sendAckToSenderBackend
-9. sendAckToSenderFrontend
-
+0.  Hi sender & Hi receiver - both connected
+1.  Packet forms at senderFrontend
+1.  SendPacketToMiddleLayerBackend
+1.  SendPacketToMiddleLayerFrontend
+1.  SendPacketToReceiverBackend
+1.  sendPacketToReceiverFrontend
+1.  sendAckToMiddleLayerBackend
+1.  sendAckToMiddleLayerFrontend
+1.  sendAckToSenderBackend
+1.  sendAckToSenderFrontend
 
 ## How to run
 
-- git clone the repo
-- navigate to directory
-- make sure you have pipenv installed, or run `brew install pipenv`
-- type `pipenv shell` to activate the virtual environment
-- install requirements from requirements.txt or pip lock file
-- run `python app.py` and navigate to `localhost:5000` in browser
-- disable debugging in app.py if you don't want messages in terminal
+* git clone the repo
+* navigate to directory
+* make sure you have pipenv installed, or run `brew install pipenv`
+* type `pipenv shell` to activate the virtual environment
+* install requirements from requirements.txt or pip lock file
+* run `python app.py` and navigate to `localhost:5000` in browser
+* disable debugging in app.py if you don't want messages in terminal
--- app.py
+++ app.py
@@ -5,17 +5,206 @@ from flask_socketio import SocketIO, emit, disconnect
 import time
 import random
 
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on installed packages.
+# Set this variable to "threading", "eventlet" or "gevent" 
+# I used gevent
 async_mode = None
 
+# #############################  ### Initialise flask ######################################
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+# ################################# Establish Connection  #####################################
+
+# Accepting connection from client
+@socketio.on('connect')
+def server_coming_alive():
+    """
+    From : Predefined event connect. Called when server comes alive.
+    Task : Initialise session variables
+    To   : Server start message at frontend
+    """
+    session['currentPacket'] = 0
+    session['currentAck'] = 0
+    emit('server_started')
+    # emit('complete_connection', {'data': 'Hi Receiver!'})
+
+
+@socketio.on('connectionRequestToMiddleLayerBackend')
+def connection_request_to_middle_layer_backend(message):
+    """
+    From : Receiver frontend after receiver said hi to sender
+    Task : Simply pass data from receiver front end to middle layer frontend
+    To   : Middle layer frontend - to display log
+    """
+    emit('connectionRequestToMiddleLayerFrontend', {'data': message['data']})
+
+
+@socketio.on('connectionRequestToSenderBackend')
+def connection_request_to_sender_backend(message):
+    """
+    From : Middle layer frontend
+    Task : If receiver greets, then all good. Else, fail connection
+    To   : Sender frontend - to display log
+    """
+    if message['data'] == 'Hi Sender!':
+        emit('connectionRequestToSenderFrontend', {
+             'data': 'Connection established. Hello Receiver!'})
+    else:
+        emit('connection_failure', {'data': 'Connection denied, Retry!'})
+
+
+# ###################################### Ping Pong #######################################
+
+@socketio.on('HeyPing')
+def ping_pong():
+    """
+    From : ping
+    Task : simply emit pong and help in roundtrip latency calculation 
+    To   : pong
+    """
+    emit('HeyPong')
+
+# ###################################### Transmission #######################################
+
+
+@socketio.on('sendPacketToSenderBackend')
+def handling_packet_at_sender_backend(message):
+    """
+    From : It's a Predefined event. Emitted by socketIO when receiver comes alive.
+    Task : Initialise a connection with sender. Say Hi to sender.
+    To   : connection request to Middle layer backend
+    """
+    session['currentPacket'] = session.get('currentPacket', 0) + 1
+    emit('sendPacketToSenderFrontend',  {
+        'data': message['data'],
+        'currentPacket': session['currentPacket']
+    })
+
+
+@socketio.on('packetTimerBlast')
+def handling_timer_Blast_from_sender(message):
+    """
+    From : Sender Timer
+    Task : If the acknowledgement of previous packet is successfully received. All good.
+           else, re transmit the message 
+    To   : Middle layer frontend
+    """
+    # careful here
+    if message['currentPacket'] <= session['currentAck']:
+        print("No issues. Packet number",
+              message['currentPacket'], "successful.")
+    else:
+        # retransmit
+        emit('sendPacketToSenderFrontend', {
+             'data': message['data'], 'currentPacket': message['currentPacket']})
+
+
+@socketio.on('SendPacketToMiddleLayerBackend')
+def handling_packet_at_middle_layer_backend(message):
+    """
+    From : sender frontend
+    Task : provide some visual delay
+    To   : Middle layer frontend
+    """
+    time.sleep(.1)
+    emit('SendPacketToMiddleLayerFrontend', {
+         'data': message['data'], 'currentPacket': message['currentPacket']})
+    time.sleep(.1)
+
+
+@socketio.on('PackedCrashedAtMiddleLayer')
+def handling_packet_crash_at_middle_layer():
+    emit('packedNotReceivedByReceiver')
+
+
+@socketio.on('sendPacketToReceiverBackend')
+def handling_packet_at_receiver_backend(message):
+    """
+    From : Middle layer frontend
+    Task : packet successfully received. Increment the ackNumber to be sent.
+    To   : Receiver frontend
+    """
+    session['currentAck'] = session.get('currentAck', 0) + 1
+    emit('sendPacketToReceiverFrontend',
+         {'data': message['data'], 'currentPacket': message['currentPacket'], 'currentAck': session['currentAck']})
+
+
+@socketio.on('sendAckToMiddleLayerBackend')
+def handling_ack_at_middle_layer_backend(message):
+    """
+    From : Receiver frontend
+    Task : packet successfully received. Increment the ackNumber to be sent
+    To   : Middle layer frontend
+    """
+    time.sleep(.1)
+    emit('sendAckToMiddleLayerFrontend', {
+         'data': message['data'], 'currentAck': message['currentAck']})
+    time.sleep(.1)
+
+
+@socketio.on('sendAckToSenderBackend')
+def handling_ack_at_sender_backend(message):
+    """
+    From : Middle Layer frontend
+    Task : Pass on the message and ackNumber
+    To   : Sender frontend
+    """
+    emit('sendAckToSenderFrontend', {
+         'data': message['data'], 'currentAck': message['currentAck']})
+
+
+# ################################# Disconnection events #################################
+
+# Requesting termination of connection
+@socketio.on('disconnect_request')
+def handling_disconnect_request_at_backend(message):
+    """
+    From : User requested connection termination
+    Task : Tie the loose ends, the call the disconnect() event
+    To   : disconnection frontend
+    """
+    emit('disconnecting_confirmation', {
+         'data': message['data'] + 'Disconnected!'})
+
+
+# Server disconnected
+@socketio.on('disconnect')
+def test_disconnect():
+    """
+    From : predefined disconnect event
+    Task : Disconnect the server from client
+    To   : None. Print logs to console.
+    """
+    print('Receiver disconnected', request.sid)
+
+
+# ################################# Serving index page #####################################
+
+# Server index.html
+@app.route('/')
+def index():
+    """
+    From : User navigates to "localhost:5000" in a new tab
+    Task : Serve the "templates/index.html" page to user
+    To   : None. Wait for events to start.
+    """
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+# ################################# Main function #####################################
+
+# Start the app : dev mode
+if __name__ == '__main__':
+    """
+    Event : Start the server
+    Task  : Keep the server running, debugging ON in dev mode
+    """
+    socketio.run(app, debug=True)
+
+# ###################################################################################
 
 # def background_thread():
 #     """Example of how to send server generated events to clients."""
@@ -26,149 +215,4 @@ thread_lock = Lock()
 #         socketio.emit('my_response',
 #                       {'data': 'Server generated event', 'count': count})
 
-
-# @socketio.on('my_event')
-# def test_message(message):
-#     session['receive_count'] = session.get('receive_count', 0) + 1
-#     emit('my_response',
-#          {'data': message['data'], 'count': session['receive_count']})
-
-
-@socketio.on('sendPacketToSenderBackend')
-def handling_packet_at_sender_backend(message):
-    session['currentPacket'] = session.get('currentPacket', 0) + 1
-    emit('sendPacketToSenderFrontend',  {
-        'data': message['data'],
-        'currentPacket': session['currentPacket']
-    })
-
-
-# Caught by middle layer for manipulation
-@socketio.on('SendPacketToMiddleLayerBackend')
-def packet_at_middle_layer(message):
-    """
-    Event : Message caught from server frontend 
-    Task  : Manipulate data and send to middle layer frontend
-    """
-    time.sleep(.1)
-    emit('SendPacketToMiddleLayerFrontend', {'data': message['data']})
-    time.sleep(.1)
-
-
-@socketio.on('packetTimerBlast')
-def handling_timer_Blast(message):
-    # careful here
-    if session['currentPacket'] >= message['currentPacket']+1:
-        print("No issues. Packet number",
-              message['currentPacket'], "successful.")
-    else:
-        # retransmit
-        emit('sendPacketToSenderFrontend', {
-             'data': message['data'], 'currentPacket': message['currentPacket']})
-
-
-# Message to be sent to Receiver
-@socketio.on('sendPacketToReceiverBackend')
-def packet_at_receiver_backend(message):
-    """
-    Event : Message received from server middle layer 
-    Task  : Forward the message to receiver end
-    """
-    # increment packet received number here
-    session['currentPacket'] = session.get('currentPacket', 0) + 1
-    emit('sendPacketToReceiverFrontend', {'data': message['data']})
-
-
-@socketio.on('sendAckToSenderBackend')
-def ack_at_sender_backend(message):
-    emit('sendAckToSenderFrontend', {'data': message['data']})
-
-
-@socketio.on('PackedCrashedAtMiddleLayer')
-def handling_packet_crash():
-    emit('packedNotReceivedByReceiver')
-
-
-@socketio.on('sendAckToMiddleLayerBackend')
-def ack_at_middle_layer_backend(message):
-    # will increase each time a packet is sent
-    # current packet number starts with 1
-    time.sleep(.1)
-    emit('sendAckToMiddleLayerFrontend', {'data': message['data']})
-    time.sleep(.1)
-
-
-# continous pings
-@socketio.on('HeyPing')
-def ping_pong():
-    """
-    Event : ping 
-    Task  : pong
-    """
-    emit('HeyPong')
-
-
-# Server index.html
-@app.route('/')
-def index():
-    """
-    Event : User navigates to "localhost:5000" in a new tab
-    Task  : Server the "templates/index.html" page to user
-    """
-    return render_template('index.html', async_mode=socketio.async_mode)
-
-
-# Accepting connection from client
-@socketio.on('connect')
-def complete_connection():
-    """
-    Event : Accept the connection from client (predefined)
-    Task  : To complete the connection to this client
-    """
-    session['currentPacket'] = 0
-    emit('server_started')
-    # emit('complete_connection', {'data': 'Hi Receiver!'})
-
-
-@socketio.on('connectionRequestToMiddleLayerBackend')
-def connection_request_to_middle_layer(message):
-    emit('connectionRequestToMiddleLayerFrontend', {'data': message['data']})
-
-
-@socketio.on('connectionRequestToSenderBackend')
-def connectionRequestToSenderBackend(message):
-    if message['data'] == 'Hi Sender!':
-        emit('connectionRequestToSenderFrontend', {
-             'data': 'Connection established. Hello Receiver!'})
-    else:
-        emit('connection_failure', {'data': 'Connection denied, Retry!'})
-
-
-# Requesting termination of connection
-@socketio.on('disconnect_request')
-def disconnect_request(message):
-    """
-    Event : User requested connection termination
-    Task  : Tie the loose ends, the call the disconnect() event
-    """
-    emit('disconnecting_confirmation', {
-         'data': message['data'] + 'Disconnected!'})
-
-
-# Server disconnected
-@socketio.on('disconnect')
-def test_disconnect():
-    """
-    Event  : Disconnect the server from client (predefined)
-    Task   : Receiver Disconnected
-    """
-    print('Receiver disconnected', request.sid)
-
-
-# Start the app : dev mode
-if __name__ == '__main__':
-    """
-    Event : Start the server
-    Task  : Keep the server running, debugging ON in dev mode
-    """
-    socketio.run(app, debug=True)
+# ###################################################################################
